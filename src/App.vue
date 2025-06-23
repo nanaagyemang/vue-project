46cf5860a3fdc015abd54ace8848383a535c246e
AirickX <eric.agyemang@bsystemslimited.com>
2025-06-23 16:25:52 +0000

    added my first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <Header></Header>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+
+export default {
+    components:{
+        Header,
+    }
+}
+</script>
